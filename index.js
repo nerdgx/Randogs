8f44779b37a6ae07509a20cc69bda56d6ef3cbc4
nerdgx <santianesgishner@gmail.com>
2025-02-27 15:40:22 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function fetchArtwork() {
                 let imageUrl = `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
 
                 let imgElement = document.getElementById('arts');
-                imgElement.style.display = "none"; // Hide image while loading
+                imgElement.style.display = "none";
                 imgElement.onload = function() {
-                    imgElement.style.display = "block"; // Show when fully loaded
+                    imgElement.style.display = "block";
                 };
                 imgElement.onerror = function() {
-                    imgElement.style.display = "none"; // Hide if there's an error
+                    imgElement.style.display = "none";
                 };
                 imgElement.src = imageUrl;
             } else {
@@ -26,3 +26,47 @@ function fetchArtwork() {
         })
         .catch(error => console.error('Error fetching artworks:', error));
 } 
+
+function fetchDog() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            updateImage(data.message);
+        })
+        .catch(error => console.error("Error fetching dog image:", error));
+}
+
+function fetchCat() {
+    fetch("https://api.thecatapi.com/v1/images/search")
+        .then(response => response.json())
+        .then(data => {
+            updateImage(data[0].url);
+        })
+        .catch(error => console.error("Error fetching cat image:", error));
+}
+
+function fetchQuote() {
+    fetch("https://inspirobot.me/api?generate=true") 
+        .then(response => response.text()) 
+        .then(imageUrl => {
+            updateImage(imageUrl);
+        })
+        .catch(error => console.error("Error fetching quote image:", error));
+}
+
+
+function updateImage(url) {
+    let imgElement = document.getElementById('arts');
+    imgElement.style.display = "none"; 
+
+    imgElement.onload = function() {
+        imgElement.style.display = "block"; 
+    };
+
+    imgElement.onerror = function() {
+        console.error("Error loading image.");
+        imgElement.style.display = "none"; 
+    };
+
+    imgElement.src = url;
+}
